863ead46a1166f81b908fc8625f03b7e89e51969
Moreno, Adrian <adrian@morenomartinez.com>
2015-07-16 12:42:15 +0200

    added dashboard scaffold

---
+++ dashboard/app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name dashboardApp
+ * @description
+ * # dashboardApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('dashboardApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ dashboard/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dashboardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dashboardApp
+ */
+angular.module('dashboardApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
