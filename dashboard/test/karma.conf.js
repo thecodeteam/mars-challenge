7ca473b6c675dff3fb62c7f18884501b86c1e311
Moreno, Adrian <adrian@morenomartinez.com>
2015-07-17 11:44:43 +0200

    added ng-websocket dependency

--- dashboard/test/karma.conf.js
+++ dashboard/test/karma.conf.js
@@ -33,6 +33,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/ng-websocket/ng-websocket.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
