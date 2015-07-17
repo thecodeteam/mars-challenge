e7182b9d415caca9c311d55e6f264e5387bb9f95
Moreno, Adrian <adrian@morenomartinez.com>
2015-07-17 13:07:21 +0200

    added jquery-knob for gauge charts

--- dashboard/test/karma.conf.js
+++ dashboard/test/karma.conf.js
@@ -34,6 +34,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/ng-websocket/ng-websocket.js',
+      'bower_components/jquery-knob/js/jquery.knob.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
