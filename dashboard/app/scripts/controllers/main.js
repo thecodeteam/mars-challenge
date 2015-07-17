b1f8adcc53e3f96c78cd8e909f8ffd3c14195701
Moreno, Adrian <adrian@morenomartinez.com>
2015-07-17 14:34:14 +0200

    added time series chart to display temperature and radiation history

--- dashboard/app/scripts/controllers/main.js
+++ dashboard/app/scripts/controllers/main.js
@@ -33,6 +33,37 @@ angular.module('dashboardApp')
         //'fgColor': '#f56954',
     });
 
+    var data2 = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
+
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myNewChart = new Chart(ctx).Line(data2, {
+        bezierCurve: false
+    });
+
     var ws = $websocket.$new({
       url: 'ws://localhost:8080/ws',
       protocols: []
@@ -51,6 +82,11 @@ angular.module('dashboardApp')
         $('.radiation')
           .val(data.radiation)
           .trigger('change');
+
+        myNewChart.addData([data.radiation, 10], "August");
+        myNewChart.removeData();
+        console.log(data2.datasets[0].data);
+        //myNewChart.update();
     });
 
     ws.$on('$close', function () {
