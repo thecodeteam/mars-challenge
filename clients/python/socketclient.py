320f270471e6866b25907bbda33b7bf6eccb7fd4
Butler Monterde, Patrick <pbutlerm@gmail.com>
2015-07-29 16:29:33 -0700

    updated comments
    corrected comments

--- clients/python/socketclient.py
+++ clients/python/socketclient.py
@@ -1,17 +1,16 @@
-#!/	/bin/python
-# Library to install run: sudo pip install websocket-client
-# if you encounter errors with a Six import
-# pip remove six; pip install six 
+#!/bin/python
+# Requires the following library to install: sudo pip install websocket-client
+# if you encounter errors with a Six import:
+# you can try: pip remove six; pip install six 
 
 from websocket import create_connection
 ws = create_connection("ws://localhost:8080/ws")
-
 
 var = 1
 while var == 1 :  # This constructs an infinite loop
   result =  ws.recv()
   print "Received '%s'" % result
-
+  
 ws.close()
 print "Good bye!"
 
