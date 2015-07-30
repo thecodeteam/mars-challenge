2de3e80bbe0e9998b5736bc9d3344f66e20e1f6c
Moreno, Adrian <adrian@morenomartinez.com>
2015-07-30 16:51:23 +0200

    fixed start script

--- dashboard/start.sh
+++ dashboard/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ $WS_ENDPOINT ]]; then
+if [ "$WS_ENDPOINT" ]; then
     sed -i 's,localhost:8080/ws,'$WS_ENDPOINT',g' /opt/mars-challenge/dashboard/app/scripts/controllers/main.js
 fi
 
