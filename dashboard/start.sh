e2aba81a6af6b964d2c90385db11a172f451928d
Moreno, Adrian <adrian@morenomartinez.com>
2015-07-30 16:33:42 +0200

    added start script for dashboard docker container

---
+++ dashboard/start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [[ $WS_ENDPOINT ]]; then
+    sed -i 's,localhost:8080/ws,'$WS_ENDPOINT',g' /opt/mars-challenge/dashboard/app/scripts/controllers/main.js
+fi
+
+/usr/sbin/nginx -g "daemon off;"
