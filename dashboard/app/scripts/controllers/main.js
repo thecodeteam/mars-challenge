a894a82aa7af0538cba99b7fe52cbd7530d7fc3e
Moreno, Adrian <adrian@morenomartinez.com>
2015-08-03 10:34:26 +0200

    removed unused code

--- dashboard/app/scripts/controllers/main.js
+++ dashboard/app/scripts/controllers/main.js
@@ -9,7 +9,6 @@
  */
 angular.module('dashboardApp')
   .controller('MainCtrl', ['$scope', '$websocket', function ($scope, $websocket) {
-    var maxHistory = 20;
     $scope.teams = [];
     $scope.connected = false;
     $scope.running = false;
@@ -31,37 +30,6 @@ angular.module('dashboardApp')
         'readOnly': true,
         'fgColor': '#f89406',
     });
-
-    var data2 = {
-        labels: [],
-        datasets: [
-            {
-                label: "Temperature",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: []
-            },
-            {
-                label: "Radiation",
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
-                data: []
-            }
-        ]
-    };
-
-    // var ctx = document.getElementById("history").getContext("2d");
-    // var myNewChart = new Chart(ctx).Line(data2, {
-    //     bezierCurve: false
-    // });
 
     var ws = $websocket.$new({
       url: 'ws://localhost:8080/ws',
@@ -88,11 +56,6 @@ angular.module('dashboardApp')
         $('.radiation')
           .val(data.readings.radiation)
           .trigger('change');
-
-        // myNewChart.addData([data.readings.temperature, data.readings.radiation], "");
-        // if (myNewChart.datasets[0].points.length > maxHistory){
-        //     myNewChart.removeData();
-        // }
 
         $scope.$apply(function() {
           $scope.teams = data.teams;
