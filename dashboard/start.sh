1e9ba0d97e8e6304bc5ae1d687a003247de67641
Moreno, Adrian <adrian@morenomartinez.com>
2015-08-03 12:22:53 +0200

    fixed dashboard packaging and docker container

--- dashboard/start.sh
+++ dashboard/start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 if [ "$WS_ENDPOINT" ]; then
-    sed -i 's,localhost:8080/ws,'$WS_ENDPOINT',g' /opt/mars-challenge/dashboard/app/scripts/controllers/main.js
+    FILE=$(ls -d /app/scripts/s*)
+    sed -i 's,localhost:8080/ws,'$WS_ENDPOINT',g' $FILE
 fi
 
 /usr/sbin/nginx -g "daemon off;"
