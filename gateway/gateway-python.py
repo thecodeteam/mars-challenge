b9421a6ba79ef991c6ea29fe78838cbe20e505ad
Butler Monterde, Patrick <pbutlerm@gmail.com>
2015-08-04 15:39:26 -0700

    updated python gateway
    Added the code required to complete the implementation

--- gateway/gateway-python.py
+++ gateway/gateway-python.py
@@ -5,15 +5,36 @@
 # you can try: pip remove six; pip install six
 
 from websocket import create_connection
+import requests
+import json
+
+# Global Variables
+controller_url = 'http://localhost:8080/api/readings'
+admin_password = '1234'
+auth_header = {'X-Auth-Token': admin_password}
+sensor_data = ''
 
 ws = create_connection("ws://localhost:8085/ws")
 
-while True :  # This constructs an infinite loop
-  result =  ws.recv()
-  print "Received '%s'" % result
+# curl -i -H 'X-Auth-Token: 1234' -X POST -d '{"solarFlare":true,"temperature":-100,"radiation":384}' http://localhost:8080/api/readings
+
+while True:  # This constructs an infinite loop
+
+    # Get Data from the Sensor Websocket
+    result = ws.recv()
+    sensor_data = result
+
+    # Performs the POST with form-encoded data
+    response = requests.post(url=controller_url, headers=auth_header, data=sensor_data)
+
+    if response.status_code == 200:
+        print("Data has been accepted!")
+        print ("Sending: {0:s}".format(result))
+        print ("HTTP Code: " + str(response.status_code) + " | Response: " + response.text)
+
+    else:
+        print("We got an Error")
+        print ("HTTP Code: " + str(response.status_code) + " | Response: " + response.text)
+
 
 ws.close()
-
-
-
-
