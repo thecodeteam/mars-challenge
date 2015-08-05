4ccb8c5335efe30c8de4e19fb02bf721ee9d1ed0
Patrick Butler Monterde <pbutlerm@gmail.com>
2015-08-05 13:45:18 -0700

    updated gateway python implementation
    Added logging and logic to wait for services

--- gateway/gateway-python.py
+++ gateway/gateway-python.py
@@ -3,22 +3,46 @@
 # Requires the following library to install: sudo pip install websocket-client
 # if you encounter errors with a Six import:
 # you can try: pip remove six; pip install six
+import time
 
 from websocket import create_connection
 import requests
 import json
+import logging
+import logging.config
+import settings
+import errno
+from socket import error as socket_error
+
+# Logging Initialization
+logging.config.dictConfig(settings.GATEWAY_LOGGING)
+logger = logging.getLogger("root")
+
 
 # Global Variables
 controller_url = 'http://localhost:8080/api/readings'
 admin_password = '1234'
 auth_header = {'X-Auth-Token': admin_password}
+sensor_socket = "ws://localhost:8085/ws"
 sensor_data = ''
 
-ws = create_connection("ws://localhost:8085/ws")
+
+#Check that the Sensor is ready to serve weather data
+while True:
+    try:
+        logger.info("Trying to connect to Sensor Socket: {0:s}".format(sensor_socket))
+        ws = create_connection(sensor_socket)
+        break
+    except socket_error as serror:
+        logger.info("Error (Sensor Socket): {}".format(str(serror.strerror)))
+        time.sleep(5)
+        continue
+
 
 # curl -i -H 'X-Auth-Token: 1234' -X POST -d '{"solarFlare":true,"temperature":-100,"radiation":384}' http://localhost:8080/api/readings
 
-while True:  # This constructs an infinite loop
+# Start collecting the Sensor Data and sending the data to the Game Server
+while True:
 
     # Get Data from the Sensor Websocket
     result = ws.recv()
@@ -28,13 +52,13 @@ while True:  # This constructs an infinite loop
     response = requests.post(url=controller_url, headers=auth_header, data=sensor_data)
 
     if response.status_code == 200:
-        print("Data has been accepted!")
-        print ("Sending: {0:s}".format(result))
-        print ("HTTP Code: " + str(response.status_code) + " | Response: " + response.text)
+        logger.info("Data has been accepted!")
+        logger.info("Sending: {0:s}".format(result))
+        logger.info("HTTP Code: {0}  | Response: {1}".format(str(response.status_code), response.text))
 
     else:
-        print("We got an Error")
-        print ("HTTP Code: " + str(response.status_code) + " | Response: " + response.text)
+        logger.error("We got an Error")
+        logger.error("HTTP Code: {0}  | Response: {1}".format(str(response.status_code), response.text))
 
 
 ws.close()
