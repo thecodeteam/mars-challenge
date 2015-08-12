e5a82284a8ccd8dce1bfbf30e8ec7e943af252e0
msalem2 <magdy.salem@emc.com>
2015-08-11 20:29:37 -0700

    Update generator.go

--- sensor-client/generator.go
+++ sensor-client/generator.go
@@ -12,7 +12,7 @@ type JSONTime time.Time
 
 func (t *JSONTime)MarshalJSON() ([]byte, error) {
     //do your serializing here
-    stamp := fmt.Sprint(time.Now().Format("2006-01-02T15:04:05z07:00"))
+    stamp := fmt.Sprintf("\"%s\"",time.Now().Format("2006-01-02T15:04:05z07:00"))
     return []byte(stamp), nil
 }
 
