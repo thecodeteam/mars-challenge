3db870ffa5135c45661558edb5a823dfadf1cae2
Patrick Butler Monterde <pbutlerm@gmail.com>
2015-08-13 18:05:19 -0700

    Update harness to run on 3 minute loop

--- game-controller/tests/game-controller-harness.py
+++ game-controller/tests/game-controller-harness.py
@@ -130,143 +130,146 @@ def team_shield_down(team_name, team_auth):
         print ('Server: Team: ' + team_name + ' Shield DOWN! request Failed!')
         print ("HTTP Code: " + str(shield_down.status_code) + " | Response: " + shield_down.text)
 
-# Test Harness ------------------------------------------------
-print("Starting the Test Harness")
-print("-------------------------")
 
-print("\nChecking the Server Status...")
-# Check that Server has not started
-if server_check_game_started(server_url):
-    print("...previous game running....")
-    server_stop(server_url)
-    server_reset(server_url)
-else:
-    print("...cleaning up....")
-    server_stop(server_url)
-    server_reset(server_url)
-
-time.sleep(2)
-
-# Testing Server Configuration Functionality
-# ------------------------------------------------
-
-print("\nChecking the Server Configuration")
-print("------------------------------------")
-server_config(server_url)
-
-# Testing Adding Teams to Game Functionality
-# ------------------------------------------------
-
-print("\nAdding Teams")
-print("--------------")
-
-# Adding team: TheBorg
-team1_name = 'TheBorgs'
-team1_auth = ''
-
-url = server_url + '/join/' + team1_name
-payload = ''
-# POST with form-encoded data
-response = requests.post(url, data=payload)
-
-team1_auth = response.text
-
-if response.status_code == 200:
-    print ('Team \'' + team1_name + '\' joined the game!')
-    print (team1_name + ' authentication Code: ' + team1_auth)
-else:
-    print ('Team \'' + team1_name + '\' joining game Failed!')
-    print ("HTTP Code: " + str(response.status_code) + " | Response: " + response.text)
-time.sleep(2)
-
-# Adding team: QuickFandango
-team2_name = 'QuickFandango'
-team2_auth = ''
-
-url = server_url + "/join/" + team2_name
-payload = ''
-# POST with form-encoded data
-response = requests.post(url, data=payload)
-
-team2_auth = response.text
-
-if response.status_code == 200:
-    print ('Team \'' + team2_name + '\' joined the game!')
-    print (team2_name + ' authentication Code: ' + team2_auth)
-else:
-    print ('Team \'' + team2_name + '\' joining game Failed!')
-    print ("HTTP Code: " + str(response.status_code) + " | Response: " + response.text)
-time.sleep(2)
-
-# Adding team: InTheBigMessos
-team3_name = 'InTheBigMessos'
-team3_auth = ''
-
-url = server_url + "/join/" + team3_name
-payload = ''
-# POST with form-encoded data
-response = requests.post(url, data=payload)
-
-team3_auth = response.text
-
-if response.status_code == 200:
-    print ('Team \'' + team3_name + '\' joined the game!')
-    print (team3_name + ' authentication Code: ' + team3_auth)
-else:
-    print ('Team \'' + team3_name + '\' joining game Failed!')
-    print ("HTTP Code: " + str(response.status_code) + " | Response: " + response.text)
-
-
-# Testing Kick Team Functionality
-# ------------------------------------------------
-
-print("\nChecking the Server Kick Functionality")
-print("----------------------------------------")
-print("Kicking {0}  team out...".format(team1_name))
-print("Team {0} has Auth Key: {1}".format(team1_name, str(team1_auth)))
-
-server_kick_team(server_url, team1_name)
-
-print("Adding  {0}  team back in...".format(team1_name))
-
-url = server_url + '/join/' + team1_name
-payload = ''
-# POST with form-encoded data
-response = requests.post(url, data=payload)
-
-team1_auth = response.text
-
-if response.status_code == 200:
-    print ('Team \'' + team1_name + '\' joined the game!')
-    print (team1_name + ' authentication Code: ' + team1_auth)
-else:
-    print ('Team \'' + team1_name + '\' joining game Failed!')
-    print ("HTTP Code: " + str(response.status_code) + " | Response: " + response.text)
-
-time.sleep(10)
-
-
-# Starting the the GAME
-# ------------------------------------------------
-
-print("\nStarting the Game")
-print("-------------------")
-# Starting the Game Server
-server_start(server_url)
-
-# Starting the Teams Logic
 while True:
-    team = random.randrange(0, 3)
-    action = random.randrange(1, 3)
-    team_list = [(team1_name, team1_auth), (team2_name, team2_auth), (team3_name, team3_auth)]
-    # print("\nGameMove: Team: " + team_list[team][0] + ' Action:' + str(action) + ' Name: ' + team_list[team][0] +'|'+ team_list[team][1])
-    if action > 1:
-        print("\nGameMove: Team: " + team_list[team][0] + ' Action: Shield UP! | Team Key: ' + team_list[team][1])
-        team_shield_up(team_list[team][0], (team_list[team][1]))
+
+    # Test Harness ------------------------------------------------
+    print("Starting the Test Harness")
+    print("-------------------------")
+
+    print("\nChecking the Server Status...")
+    # Check that Server has not started
+    if server_check_game_started(server_url):
+        print("...previous game running....")
+        server_stop(server_url)
+        server_reset(server_url)
     else:
-        print("\nGameMove: Team: " + team_list[team][0] + ' Action: Shield Down! | Team Key: ' + team_list[team][1])
-        team_shield_down(team_list[team][0], (team_list[team][1]))
+        print("...cleaning up....")
+        server_stop(server_url)
+        server_reset(server_url)
+
     time.sleep(2)
-    if server_check_game_started(server_url) == False:
-        print('\nServer: Game is Over...')
-        break
+
+    # Testing Server Configuration Functionality
+    # ------------------------------------------------
+
+    print("\nChecking the Server Configuration")
+    print("------------------------------------")
+    server_config(server_url)
+
+    # Testing Adding Teams to Game Functionality
+    # ------------------------------------------------
+
+    print("\nAdding Teams")
+    print("--------------")
+
+    # Adding team: TheBorg
+    team1_name = 'TheBorgs'
+    team1_auth = ''
+
+    url = server_url + '/join/' + team1_name
+    payload = ''
+    # POST with form-encoded data
+    response = requests.post(url, data=payload)
+
+    team1_auth = response.text
+
+    if response.status_code == 200:
+        print ('Team \'' + team1_name + '\' joined the game!')
+        print (team1_name + ' authentication Code: ' + team1_auth)
+    else:
+        print ('Team \'' + team1_name + '\' joining game Failed!')
+        print ("HTTP Code: " + str(response.status_code) + " | Response: " + response.text)
+    time.sleep(2)
+
+    # Adding team: QuickFandango
+    team2_name = 'QuickFandango'
+    team2_auth = ''
+
+    url = server_url + "/join/" + team2_name
+    payload = ''
+    # POST with form-encoded data
+    response = requests.post(url, data=payload)
+
+    team2_auth = response.text
+
+    if response.status_code == 200:
+        print ('Team \'' + team2_name + '\' joined the game!')
+        print (team2_name + ' authentication Code: ' + team2_auth)
+    else:
+        print ('Team \'' + team2_name + '\' joining game Failed!')
+        print ("HTTP Code: " + str(response.status_code) + " | Response: " + response.text)
+    time.sleep(2)
+
+    # Adding team: InTheBigMessos
+    team3_name = 'InTheBigMessos'
+    team3_auth = ''
+
+    url = server_url + "/join/" + team3_name
+    payload = ''
+    # POST with form-encoded data
+    response = requests.post(url, data=payload)
+
+    team3_auth = response.text
+
+    if response.status_code == 200:
+        print ('Team \'' + team3_name + '\' joined the game!')
+        print (team3_name + ' authentication Code: ' + team3_auth)
+    else:
+        print ('Team \'' + team3_name + '\' joining game Failed!')
+        print ("HTTP Code: " + str(response.status_code) + " | Response: " + response.text)
+
+
+    # Testing Kick Team Functionality
+    # ------------------------------------------------
+
+    print("\nChecking the Server Kick Functionality")
+    print("----------------------------------------")
+    print("Kicking {0}  team out...".format(team1_name))
+    print("Team {0} has Auth Key: {1}".format(team1_name, str(team1_auth)))
+
+    server_kick_team(server_url, team1_name)
+
+    print("Adding  {0}  team back in...".format(team1_name))
+
+    url = server_url + '/join/' + team1_name
+    payload = ''
+    # POST with form-encoded data
+    response = requests.post(url, data=payload)
+
+    team1_auth = response.text
+
+    if response.status_code == 200:
+        print ('Team \'' + team1_name + '\' joined the game!')
+        print (team1_name + ' authentication Code: ' + team1_auth)
+    else:
+        print ('Team \'' + team1_name + '\' joining game Failed!')
+        print ("HTTP Code: " + str(response.status_code) + " | Response: " + response.text)
+
+    time.sleep(180)
+
+
+    # Starting the the GAME
+    # ------------------------------------------------
+
+    print("\nStarting the Game")
+    print("-------------------")
+    # Starting the Game Server
+    server_start(server_url)
+
+    # Starting the Teams Logic
+    while True:
+        team = random.randrange(0, 3)
+        action = random.randrange(1, 3)
+        team_list = [(team1_name, team1_auth), (team2_name, team2_auth), (team3_name, team3_auth)]
+        # print("\nGameMove: Team: " + team_list[team][0] + ' Action:' + str(action) + ' Name: ' + team_list[team][0] +'|'+ team_list[team][1])
+        if action > 1:
+            print("\nGameMove: Team: " + team_list[team][0] + ' Action: Shield UP! | Team Key: ' + team_list[team][1])
+            team_shield_up(team_list[team][0], (team_list[team][1]))
+        else:
+            print("\nGameMove: Team: " + team_list[team][0] + ' Action: Shield Down! | Team Key: ' + team_list[team][1])
+            team_shield_down(team_list[team][0], (team_list[team][1]))
+        time.sleep(2)
+        if server_check_game_started(server_url) == False:
+            print('\nServer: Game is Over...')
+            break
