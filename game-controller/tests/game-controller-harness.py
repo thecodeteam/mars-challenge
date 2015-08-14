bda6000442f7565d77218b96c72e701456849808
Patrick Butler Monterde <pbutlerm@gmail.com>
2015-08-13 17:59:05 -0700

    Revert "Adding Docker file for game harness and taking environment variables"
    This reverts commit cca30fb5e8e80afad525ccde3b61a4419c534874.

--- game-controller/tests/game-controller-harness.py
+++ game-controller/tests/game-controller-harness.py
@@ -6,15 +6,10 @@
 import requests  # https://github.com/kennethreitz/requests/
 import time
 import random
-import os
 
-
-
-# Global Variables
-controller_endpoint = os.environ['GC_ENDPOINT']
 
 #Replace this variables as appropriate
-server_url = 'http://' + controller_endpoint + '/api'
+server_url = 'http://192.168.59.103:8080/api'
 admin_header = {'X-Auth-Token': '1234'}
 
 
